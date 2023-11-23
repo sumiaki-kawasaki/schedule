e475ffbc1b1a12b37cca2cbf7f1fcf5bc9aa844b
Sumiaki Kawasaki <cb_clear_bright@yahoo.co.jp>
2023-11-23 14:56:52 +0000

    途中コミット

--- src/lib/components/gantt/Gantt.svelte
+++ src/lib/components/gantt/Gantt.svelte
@@ -1,6 +1,29 @@
-<div class="w-full sm:flex">
-	<div class="container mx-auto">
-		<div>グリッド</div>
-		<div>タスク</div>
+<div class="w-full">
+	<div class="flex">
+    <div class="w-96">
+      <div class="flex justify-center items-center h-12">
+        <div class="border w-4/6 h-full">
+          <div class="text-center px-0 py-0 m-auto">
+            題名
+          </div>
+        </div>
+        <div class="border w-2/6">
+          <div class="text-center">
+            担当者
+          </div>
+        </div>
+      </div>
+      <div class="border">
+        グリッド
+      </div>
+    </div>
+    <div class="w-full">
+      <div class="border h-12">
+        タスクヘッダ
+      </div>
+      <div class="border">
+        タスク
+      </div>
+    </div>
 	</div>
 </div>
