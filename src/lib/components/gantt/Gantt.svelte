9f59eeefec0f5d8f6cb2266b05e4df9555214f07
Sumiaki Kawasaki <cb_clear_bright@yahoo.co.jp>
2023-12-02 07:34:34 +0000

    タスク欄仮実装

--- src/lib/components/gantt/Gantt.svelte
+++ src/lib/components/gantt/Gantt.svelte
@@ -1,6 +1,9 @@
 <script>
+	import { task } from '$lib/repositories/task';
 	import { BetweenDate } from './BetweenDate';
 	import MonthColumn from './MonthColumn.svelte';
+	import TaskColumn from './TaskColumn.svelte';
+
 	const today = new Date();
 	// const beginDate = new Date(today);
 	const beginDate = new Date(today.getFullYear(), today.getMonth(), 1);
@@ -17,7 +20,7 @@
 </script>
 
 <div class="w-full">
-	<div class="flex overflow-auto">
+	<div class="flex">
 		<div class="">
 			<div class="flex w-96 h-12">
 				<div class="border flex justify-center items-center w-4/6 h-full">
@@ -27,15 +30,23 @@
 					<p>担当者</p>
 				</div>
 			</div>
-			<div class="border w-full">グリッド</div>
+			<div class="w-full">
+				{#each $task.tasks as item}
+					<TaskColumn {item} />
+				{/each}
+				<!-- スクロールバーの分 -->
+				<div class="h-4"></div> 
+			</div>
 		</div>
-		<div class="w-full">
+		<div class="w-full overflow-auto">
 			<div class="flex">
 				{#each months as between}
 					<MonthColumn {between} />
 				{/each}
+				<!-- スクロールバーの分 -->
+				<div class="w-4"></div> 
 			</div>
-			<div class="border">タスク</div>
+			<div></div>
 		</div>
 		<div class="absolute left-96 w-2 h-full cursor-col-resize"></div>
 	</div>
---
+++ src/lib/components/gantt/TaskColumn.svelte
@@ -0,0 +1,8 @@
+<script>
+	/** @type {import('$lib/repositories/task').TaskItem} */
+	export let item;
+</script>
+
+<div class="border">
+	<p>{item.name}</p>
+</div>
